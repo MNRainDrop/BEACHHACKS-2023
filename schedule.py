9d782ef994a69ae31ad7abc626cd9cc98360e5be
JheneJin <vinjin000@gmail.com>
2023-02-25 22:37:10 -0800

    Update schedule.py
    worked on addTask and sleepNeeded

--- schedule.py
+++ schedule.py
@@ -1,6 +1,9 @@
 
 #user inputs time they want to sleep
 def sleepNeeded():
+    sleepInput = int(input("Please enter amount of sleep, you would need."))
+    sleep = 24 - sleepInput
+    return sleep
 
 #view task
 def selectTask():
@@ -11,12 +14,26 @@ def selectTask():
 #param: 
 #output: 
 def addTask():
+    userTask = string(input("Please enter your task name. "))
+    print("Task are categorized into class, homework, hobby, chore, work.")
+    category = string(input("Please input the category it would go under. "))
+    while (category != "class") or (category != "homework") or (category != "hobby") or (category != "chore") or (category != "work")
+    if category == "class":
+        days = string(input("Please enter the days when your class will take place. "))
+        startTimes = int(input("Please enter the start time of your class. "))
+        endTime = int(input("Please enter the end time of your class. "))
+        
 
+    taskDate = string(input("Please enter a date for when you will start the task. "))
 #be able to delete tasks
 def deleteTask():
 
+
 #edit tasks to be more versatile with time
 def editTask():
+
+def scheduleTask():
+
 
 
 
