e4ccc204798d46afe1d5bee8320afefdc8976bc1
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-02-25 21:36:56 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+
+layout = []
+
+window = sg.Window("Demo", layout)
+
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
