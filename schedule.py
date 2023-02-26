3f8fa5cde2f4065fc7732a5d79c167c346c731d1
JheneJin <vinjin000@gmail.com>
2023-02-25 23:29:01 -0800

    Update schedule.py
    change string to str

--- schedule.py
+++ schedule.py
@@ -4,12 +4,12 @@ def sleepNeeded():
     sleep = 24 - sleepInput
     return sleep
 
-def sleepNeeded(input: int):
-    pause
+#def sleepNeeded(input: int):
+    #pause
 
 #view task
-def selectTask():
-    print("due date:", due, "start time", start, "end time", end, "category")
+#def selectTask():
+    #print("due date:", due, "start time", start, "end time", end, "category")
 
 
 #add tasks to the schedule
@@ -17,20 +17,28 @@ def selectTask():
 #param: due date: str, start time, end time, category:
 #output: 
 def addTask():
-    userTask = string(input("Please enter your task name. "))
+    userTask = str(input("Please enter your task name. "))
     print("Task are categorized into class, homework, hobby, chore, work.")
-    category = string(input("Please input the category it would go under. "))
+    category = str(input("Please input the category it would go under. "))
     while (category != "class") or (category != "homework") or (category != "hobby") or (category != "chore") or (category != "work"):
         print("Invalid Input, please enter again one of these categories: class, homework, hobby, chore, work")
-        category = string(input("Please input the category it would go under. "))
-        if category == "class":
-            day = string(input("Please enter the day when your class will take place. "))
+        category = str(input("Please input the category it would go under. "))
+        if category == "class" or category == "chore" or category == "hobby" or category == "work" :
+            day = str(input("Please enter the day when your class will take place. "))
             startTime = int(input("Please enter the start time of your class. "))
             endTime = int(input("Please enter the end time of your class. "))
-            date = string(input("Please enter a date for when you will start the task. "))
+            date = str(input("Please enter a date for when you will start the task. "))
             taskDict = {"Task": userTask, "Category": category, "Day": day, "startTime": startTime, "endTime": endTime, "Date": date}
-            break
-
+            return taskDict
+        elif category == "homework":
+            day = str(input("Please enter the day when your class will take place. "))
+            startTime = int(input("Please enter the start time of your class. "))
+            endTime = int(input("Please enter the end time of your class. "))
+            date = str(input("Please enter a date for when you will start the task. "))
+            taskDict = {"Task": userTask, "Category": category, "Day": day, "startTime": startTime, "endTime": endTime, "Date": date}
+            return taskDict
+        
+        
 
 #user enters true false
 #be able to delete tasks
@@ -41,8 +49,8 @@ def deleteTask():
 
 
 #edit tasks to be more versatile with time
-def editTask():
+#def editTask():
 
-def scheduleTask():
-    pause
 
+#def scheduleTask():
+addTask()
