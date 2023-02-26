2fe601abf6157e797b8660a79e4be9e116de8f81
euilee0129 <superbankaisam@gmail.com>
2023-02-26 01:15:01 -0800

    owo
    updated 2 brains > 1

--- schedule.py
+++ schedule.py
@@ -1,58 +1,62 @@
-#user inputs time they want to sleep
-def sleepNeeded():
-    sleepInput = int(input("Please enter amount of sleep, you would need."))
-    sleep = 24 - sleepInput
-    return sleep
-
-#def sleepNeeded(input: int):
-    #pause
+# #user inputs time they want to sleep
+# #output: time spent awake
+# def sleepNeeded():
+#     sleepInput = int(input("Please enter amount of sleep, you would need."))
+#     sleep = 24 - sleepInput
+#     return sleep
 
 #view task
-#def selectTask():
-    #print("due date:", due, "start time", start, "end time", end, "category")
+#output: String of all values
+def selectTask():
+    temp = addTask()
+    for val in temp:
+        print(val, temp[val])
 
 
 #add tasks to the schedule
 #tasks are categorized into classes, homework/projects, hobbies
-#param: due date: str, start time, end time, category:
-#output: 
+#output: returns dictionary of all values
 def addTask():
-    userTask = str(input("Please enter your task name. "))
-    print("Task are categorized into class, homework, hobby, chore, work.")
     category = str(input("Please input the category it would go under. "))
-    while (category != "class") or (category != "homework") or (category != "hobby") or (category != "chore") or (category != "work"):
+    print("Task are categorized into class, homework, hobby, chore, work.")
+    while (category != "class") and (category != "homework") and (category != "hobby") and (category != "chore") and (category != "work"):
         print("Invalid Input, please enter again one of these categories: class, homework, hobby, chore, work")
         category = str(input("Please input the category it would go under. "))
-        if category == "class" or category == "chore" or category == "hobby" or category == "work" :
-            day = str(input("Please enter the day when your class will take place. "))
-            startTime = int(input("Please enter the start time of your class. "))
-            endTime = int(input("Please enter the end time of your class. "))
-            date = str(input("Please enter a date for when you will start the task. "))
-            taskDict = {"Task": userTask, "Category": category, "Day": day, "startTime": startTime, "endTime": endTime, "Date": date}
-            return taskDict
-        elif category == "homework":
-            day = str(input("Please enter the day when your class will take place. "))
-            startTime = int(input("Please enter the start time of your class. "))
-            endTime = int(input("Please enter the end time of your class. "))
-            date = str(input("Please enter a date for when you will start the task. "))
-            taskDict = {"Task": userTask, "Category": category, "Day": day, "startTime": startTime, "endTime": endTime, "Date": date}
-            return taskDict
-        
+    if category == "class" or category == "chore" or category == "hobby" or category == "work" :
+        return getValue(category)
+    elif category == "homework":
+        return getValue(category)
         
 
-#user enters true false
-#be able to delete tasks
-def deleteTask():
-    print("would you like to delete task? Y/N")
-    complete = False
-    moved = False
+#auxiliary function
+def getValue(category):
+    userTask = str(input("Please enter your task name. "))
+    day = str(input("Please enter the day when your class will take place. "))
+    startTime = int(input("Please enter the start time of your class. "))
+    endTime = int(input("Please enter the end time of your class. "))
+    date = str(input("Please enter a date for when you will start the task. "))
+    taskDict = {"Task": userTask, "Category": category, "Day": day, "startTime": startTime, "endTime": endTime, "Date": date}
+    return taskDict
+
+        
+# # #user enters true false to be able to delete tasks
+# # def deleteTask():
+#     # print("would you like to delete task? Y/N")
+#     # input= str(input(""))
+#     # if input == 'N':
+#     #     exit
+#     #elif input == 'Y':
 
 
-#edit tasks to be more versatile with time
-#def editTask():
+
+# # #edit tasks to be more versatile with time
+# # #def editTask():
 
 
-#def scheduleTask():
-#addTask()
-hours = sleepNeeded()
-print(hours)
+# bedTime = {}
+# #output: holds the value
+# def scheduleTask():
+#     temp = addTask()
+#     for val in temp:
+
+selectTask()
