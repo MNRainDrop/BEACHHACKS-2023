b9302acbb182f377010b1f4d2d6e7d00723133f9
JheneJin <vinjin000@gmail.com>
2023-02-25 22:02:54 -0800

    Update schedule.py
    classes

--- schedule.py
+++ schedule.py
@@ -1,3 +1,25 @@
 
-print("Welcome to _____, We help optimize time for college students, so they can have a life.")
-taskInput = 
+#user inputs time they want to sleep
+def sleepNeeded():
+
+#view task
+def selectTask():
+
+
+#add tasks to the schedule
+#tasks are categorized into classes, homework/projects, hobbies
+#param: 
+#output: 
+def addTask():
+
+#be able to delete tasks
+def deleteTask():
+
+#edit tasks to be more versatile with time
+def editTask():
+
+
+
+
+
+
