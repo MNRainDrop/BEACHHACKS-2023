5ec14bc979754951b3be73c6d0d60d19b55556f1
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-02-25 17:06:40 -0800

    Update to index.html

--- game.js
+++ game.js
@@ -1,11 +1,27 @@
-function draw() {
+var canvasWidth = 600;
+var canvasHeight = 400;
+
+function startGame() {
+    gameCanvas.start();
+
+}
+
+var gameCanvas = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = canvasWidth;
+        this.canvas.height = canvasHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+function update() {
     const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
 
         ctx.fillStyle = "green";
-        ctx.fillRect(0, 0, screen.width, screen.height);
+        ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
     }
 }
-
-
