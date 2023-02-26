ab53c278b70b95af195d89755a0a56a5e63fc0a4
euilee0129 <superbankaisam@gmail.com>
2023-02-25 22:26:29 -0800

    Update schedule.py

---
+++ schedule.py
@@ -0,0 +1,34 @@
+
+#user inputs time they want to sleep
+def sleepNeeded(input: int):
+
+#view task
+def selectTask():
+    print("due date:"+due+"start time"+start+"end time"+end+"category")
+
+
+#add tasks to the schedule
+#tasks are categorized into classes, homework/projects, hobbies
+#param: due date: str, start time, end time, category:
+#output: 
+def addTask():
+
+
+#user enters true false
+#be able to delete tasks
+def deleteTask():
+    print("would you like to delete task? Y/N")
+    complete = False
+    moved = False
+
+
+
+
+#edit tasks to be more versatile with time
+def editTask():
+
+
+
+
+
+
